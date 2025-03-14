66e16ea0f00c90f3eeb92359b6605ff887d94a38
jasonkaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-03-14 23:46:26 +0100

    Login Page + Responsive

--- frontend/assets/js/router.js
+++ frontend/assets/js/router.js
@@ -3,5 +3,6 @@ export const routes = {
     'home' : 'home.html',
     'report' : 'report.html',
     'profile' : 'profile.html',
-    'admin' : 'admin.html'
+    'admin' : 'admin.html',
+    'login' : 'login.html'
 }
