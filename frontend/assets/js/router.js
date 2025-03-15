fb72fe2a42d8446d388cb48be0453d49f47ca8f7
jasonkaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-03-15 11:37:53 +0100

    Register Page + Responsive

--- frontend/assets/js/router.js
+++ frontend/assets/js/router.js
@@ -4,5 +4,6 @@ export const routes = {
     'report' : 'report.html',
     'profile' : 'profile.html',
     'admin' : 'admin.html',
-    'login' : 'login.html'
+    'login' : 'login.html',
+    'register' : 'register.html'
 }
