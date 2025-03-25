578545242ac900ec32aa5c6e531648ba9dbcd446
jasonkaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-03-25 11:50:05 +0100

    userDao

--- backend/rest/dao/usersDao.php
+++ backend/rest/dao/usersDao.php
@@ -11,13 +11,22 @@ class User extends BaseDao {
     public function __construct(){
 
         $this->table_name = "users";
-        parent::__construct($this->table_name);
+        parent::__construct("$this->table_name");
     }
 
     public function getAllUsers()
     {
         $query = "SELECT * FROM " . $this->table_name;
         return $this->query($query, []);
+    }
+
+    public function getUserByEmail($email){
+        $query = "SELECT * FROM " . $this->table_name . " WHERE email = :email";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
 
