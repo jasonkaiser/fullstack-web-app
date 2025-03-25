07fc8fd3d3e450562aea4abfbd6ff1baba0a17ef
jasonkaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-03-25 17:28:27 +0100

    Milestone 2 Finished

--- backend/rest/dao/categoriesDao.php
+++ backend/rest/dao/categoriesDao.php
@@ -15,12 +15,17 @@
         }
 
 
-        public function gettAllCategories(){
+        public function getCategories(){
             $query = "SELECT * FROM " . $this->table_name;
             return $this->query($query, []);
         }
 
-
+        public function getCategoryByName($category_name){
+            
+            $query = "SELECT * FROM " . $this->table_name . " WHERE categoryName = :categoryName";
+            $params = [":categoryName" => $category_name];
+            return $this->query($query, $params);
+        }
 
     }
 
