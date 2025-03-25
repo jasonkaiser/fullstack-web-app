45b0dd1eb722de85188eacfb6e0c2e9448a9b1e6
jasonkaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-03-25 12:14:01 +0100

    Categories Dao

---
+++ backend/rest/dao/categoriesDao.php
@@ -0,0 +1,45 @@
+<?php
+
+    require_once __DIR__ . "/baseDao.php";  
+
+
+    class Category extends BaseDao {
+
+    protected $table_name;
+
+
+        public function __construct(){
+
+            $this->table_name = "categories";
+            parent::__construct($this->table_name);
+        }
+
+
+        public function gettAllCategories(){
+            $query = "SELECT * FROM " . $this->table_name;
+            return $this->query($query, []);
+        }
+
+        public function getItemByStatus($status){
+
+        }
+
+        public function getItemByName($name){
+
+        }
+
+        public function getItemByUser($user_id){
+
+        }
+
+        public function getAllItems(){
+
+        }
+
+
+    }
+
+
+
+
+?>
--- backend/rest/test.php
+++ backend/rest/test.php
@@ -1,18 +1,29 @@
 <?php
-ini_set('display_errors', 1);   
-error_reporting(E_ALL);      
 
-require_once __DIR__ . "/dao/baseDao.php";
-require_once __DIR__ . "/dao/usersDao.php"; 
+    ini_set('display_errors', 1);   
+    error_reporting(E_ALL);      
 
-$user = new User();
-$getUser = $user->getUserByEmail("bob.brown@example.com");
+    require_once __DIR__ . "/dao/baseDao.php";
+    require_once __DIR__ . "/dao/usersDao.php";
+    require_once __DIR__ . "/dao/categoriesDao.php";  
 
-if ($getUser) {
-    // Since getUserByEmail returns a single user, you can directly access the fields
-    echo "User ID: " . $getUser['id'] . "<br>";
-    echo "User Name: " . $getUser['name'] . "<br>";
-} else {
-    echo "No user found with the given email.";
-}
+    $user = new User();
+    $getUser = $user->getUserByEmail("bob.brown@example.com");
+
+    $categories = new Category();
+    $getCategories = $categories->gettAllCategories();
+
+    if ($getUser) {
+        
+        echo "User ID: " . $getUser['id'] . "<br>";
+        echo "User Name: " . $getUser['name'] . "<br>";
+    } else {
+        echo "No user found with the given email.";
+    }
+
+    foreach( $getCategories as $category ) {
+        echo "<td>". $category["categoryID"] . "</td>";
+        echo "<td>". $category["categoryName"] . "</td>";
+    }
+    
 ?>
