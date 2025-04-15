a63aac9dd33f34252e6141e1a4b540a90acd85f2
Jason Kaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-04-15 11:24:49 +0200

    Swagger without Annotations

---
+++ backend/public/v1/docs/swagger.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/../../../rest/vendor/autoload.php';
+
+define('LOCALSERVER', 'http://localhost/fullstack-web-app/backend/');
+define('PRODSERVER', 'https://add-production-server-after-deployment/backend/');
+
+if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
+    define('BASE_URL', 'http://localhost/fullstack-web-app/backend/');
+} else {
+    define('BASE_URL', 'https://add-production-server-after-deployment/backend/');
+}
+
+$openapi = \OpenApi\Generator::scan([
+    __DIR__ . '/doc_setup.php',
+    __DIR__ . '/../../../rest/routes'
+]);
+header('Content-Type: application/json');
+echo $openapi->toJson();
+?>
