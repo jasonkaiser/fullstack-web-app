0ff519e3d9c04fb0648177ba44e1cdb3d28bbb0f
Jason Kaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-06-06 16:44:33 +0200

    Deployment trying to fix

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -8,7 +8,7 @@ define('PRODSERVER', 'https://shark-app-5hzgo.ondigitalocean.app/backend/rest');
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost/fullstack-web-app/backend/');
 } else {
-    define('BASE_URL', $_SERVER['SERVER_NAME'] . 'https://shark-app-5hzgo.ondigitalocean.app/backend/');
+    define('BASE_URL', 'https://shark-app-5hzgo.ondigitalocean.app/backend/');
 }
 
 $openapi = \OpenApi\Generator::scan([
